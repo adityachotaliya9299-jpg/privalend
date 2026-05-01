4b7c7f0e918dc3fe8c622dfb571cc0d06efe33f8
Aditya <adityachotaliya9299@gmail.com>
2026-05-01 17:11:28 +0000

    feat: export all instruction modules

---
+++ programs/privalend/src/instructions/mod.rs
@@ -0,0 +1,13 @@
+pub mod initialize;
+pub mod deposit;
+pub mod borrow;
+pub mod repay;
+pub mod liquidate;
+pub mod dwallet;
+
+pub use initialize::*;
+pub use deposit::*;
+pub use borrow::*;
+pub use repay::*;
+pub use liquidate::*;
+pub use dwallet::*;
